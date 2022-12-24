a5101079972dbe91bdacb303e86989f4cca5a724
IronHymen <chris.sagresnaw@gmail.com>
2022-12-24 01:06:13 -0400

    got rid of the counter, changed output to display time instead

--- thumbnailRefresh.py
+++ thumbnailRefresh.py
@@ -1,10 +1,10 @@
 import asyncio
-import os
 from datetime import datetime
 from pathlib import Path
 from blinkpy.auth import Auth
 from blinkpy.blinkpy import Blink
 from blinkpy.helpers.util import json_load
+import cv2
 
 # Set up the Blink and Auth objects
 blink = Blink()
@@ -12,7 +12,28 @@ auth = Auth(json_load("cred.json"))
 blink.auth = auth
 blink.start()
 
-counter_file = "count.txt"
+# Define a function to add a timestamp to an image
+
+
+def add_timestamp(image, timestamp_text):
+    # Get the size of the image
+    image_height, image_width, _ = image.shape
+
+    # Set the font and scale of the timestamp text
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    scale = 2  # Increase the scale to make the text larger
+    thickness = 1
+
+    # Get the size of the text
+    text_size = cv2.getTextSize(timestamp_text, font, scale, thickness)[0]
+
+    # Set the bottom-left corner of the text
+    text_x = image_width - text_size[0] - 10
+    text_y = image_height - 10
+
+    # Add the timestamp text to the image
+    cv2.putText(image, timestamp_text, (text_x, text_y), font,
+                scale, (255, 255, 255), thickness, cv2.LINE_AA)
 
 
 async def update_image(camera):
@@ -21,20 +42,25 @@ async def update_image(camera):
 
 
 async def save_image(camera, image_path):
+    # Save the image to file using the camera.image_to_file method
     camera.image_to_file(image_path)
-    print("Image saved to file")
+
+    # Load the saved image
+    image = cv2.imread(str(image_path))
+
+    # Get the current timestamp
+    now = datetime.now()
+    timestamp_text = now.strftime("%Y/%m/%d %H:%M")
+
+    # Add the timestamp to the image
+    add_timestamp(image, timestamp_text)
+
+    # Save the image with the timestamp to file
+    cv2.imwrite(str(image_path), image)
+    print(f"Image saved to file: {image_path}")
 
 
 async def main():
-    global counter  # Make the counter variable global
-    counter = 0
-
-    # Load the counter value from the file, if it exists
-    if os.path.exists(counter_file):
-        with open(counter_file, "r") as f:
-            counter = int(f.read())
-        print(f"Loaded counter value from file: {counter}")
-
     # Get the current date and create the directory for storing images
     today = datetime.now()
     image_dir = Path("storage") / today.strftime("%Y%m%d")
@@ -57,19 +83,13 @@ async def main():
         await asyncio.sleep(10)  # Wait 10 seconds before saving the image
 
         # Save the image to the appropriate directory
-        image_path = camera_dir / f"{name}{counter:02}.jpg"
+        image_path = camera_dir / f"{name}_{today.strftime('%m%d%H%M')}.jpg"
         await save_image(camera, image_path)
         print(
             f"Saved image to {image_path} at {today.strftime('%Y/%m/%d %H:%M:%S')}"
         )
         # Wait 5 seconds before moving on to the next camera
         await asyncio.sleep(5)
-
-    # Increment the counter and save it to the file
-    counter += 1
-    with open(counter_file, "w") as f:
-        f.write(str(counter))
-    print(f"Saved counter value to file: {counter}")
 
 # Run the main function
 asyncio.run(main())
